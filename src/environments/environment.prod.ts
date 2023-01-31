803eb0134fc91049620dd30287f07a6353aa388c
Soumayjit Chakraborty <soumyo.chak1999@gmail.com>
2023-01-31 16:24:14 +0530

    changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://openweather43.p.rapidapi.com/weather',
-  XRapidAPIKey: 'X-RapidAPI-Key',
-  XRapidAPIKeyValue: '2098454712msh117ee6475cd413fp104705jsn79a822207b21',
-  XRapidAPIHost: 'X-RapidAPI-Host',
-  XRapidAPIHostValue: 'openweather43.p.rapidapi.com',
+  // baseUrl: 'https://openweather43.p.rapidapi.com/weather',
+  // XRapidAPIKey: 'X-RapidAPI-Key',
+  // XRapidAPIKeyValue: '2098454712msh117ee6475cd413fp104705jsn79a822207b21',
+  // XRapidAPIHost: 'X-RapidAPI-Host',
+  // XRapidAPIHostValue: 'openweather43.p.rapidapi.com',
 }
