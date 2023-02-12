d6ac45585e8e460bb2c6b6def01f6a3c45076f57
Soumayjit Chakraborty <soumyo.chak1999@gmail.com>
2023-02-12 14:58:30 +0530

    design and layout is changed

--- src/app/Models/WeatherModel.model.ts
+++ src/app/Models/WeatherModel.model.ts
@@ -1,68 +1,53 @@
-/* A TypeScript interface. */
-// export interface WeatherData {
-//   cloud_pct: number
-//   temp: number
-//   feels_like: number
-//   humidity: number
-//   min_temp: number
-//   max_temp: number
-//   wind_speed: number
-//   wind_degrees: number
-//   sunrise: number
-//   sunset: number
-// }
-
-/* Defining the structure of the data that is returned from the API. */
 export interface Coord {
-  lon: number
-  lat: number
+  lon: number;
+  lat: number;
 }
 
 export interface Weather {
-  id: number
-  main: string
-  description: string
-  icon: string
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
 }
 
 export interface Main {
-  temp: number
-  feels_like: number
-  temp_min: number
-  temp_max: number
-  pressure: number
-  humidity: number
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
 }
 
 export interface Wind {
-  speed: number
-  deg: number
+  speed: number;
+  deg: number;
 }
 
 export interface Clouds {
-  all: number
+  all: number;
 }
 
 export interface Sys {
-  type: number
-  id: number
-  country: string
-  sunrise: number
-  sunset: number
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
 }
 
 export interface WeatherData {
-  coord: Coord
-  weather: Weather[]
-  base: string
-  main: Main
-  visibility: number
-  wind: Wind
-  clouds: Clouds
-  dt: number
-  sys: Sys
-  timezone: number
-  id: number
-  name: string
-  cod: number
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
 }
--- src/app/Services/weather-services.service.ts
+++ src/app/Services/weather-services.service.ts
@@ -1,8 +1,8 @@
-import { WeatherData } from './../Models/WeatherModel.model'
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
-import { environment } from 'src/environments/environment'
+import { WeatherData } from './../Models/WeatherModel.model';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +11,10 @@ export class WeatherServicesService {
   constructor(private http: HttpClient) {}
   getWeatherData(city: string): Observable<WeatherData> {
     return this.http.get<WeatherData>(environment.baseUrl, {
-      headers: new HttpHeaders()
-        .set(environment.XRapidAPIHost, environment.XRapidAPIHostValue)
-        .set(environment.XRapidAPIKey, environment.XRapidAPIKeyValue),
       params: new HttpParams()
         .set('q', city)
-        .set('appid', 'da0f9c8d90bde7e619c3ec47766a42f4')
-        .set('appid', 'da0f9c8d90bde7e619c3ec47766a42f4')
+        .set(environment.appId, environment.appIdValue)
         .set('units', 'metric'),
-    })
+    });
   }
 }
